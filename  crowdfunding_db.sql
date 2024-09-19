b654f7d8bd92f511031d0d9a3c7036e7af720a89
Phuong Nguyen <phuongnguyen@Phuongs-MacBook-Pro.local>
2024-09-19 14:17:34 -0400

    Crowdfunding_db update

---
+++  crowdfunding_db.sql
@@ -0,0 +1,40 @@
+CREATE TABLE category (
+    category_id VARCHAR(10) PRIMARY KEY,
+    category VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE subcategory(
+	subcategory_id VARCHAR(10) PRIMARY KEY,
+	subcategory VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE contacts(
+	contact_id int PRIMARY KEY,
+	first_name VARCHAR(200) NOT NULL,
+	last_name VARCHAR(200) NOT NULL
+);
+
+ALTER TABLE contacts
+ADD COLUMN email VARCHAR(200) NOT NULL;
+
+CREATE TABLE campaign(
+	cf_id int PRIMARY KEY,
+	contact_id int REFERENCES contacts(contact_id),
+	company_name VARCHAR(200) NOT NULL,
+	description text,
+	goal FLOAT,
+	pledged FLOAT,
+	outcome VARCHAR(50),
+	backers_count int,
+	country VARCHAR(50),
+	currency VARCHAR,
+	launch_date TIMESTAMP,
+	end_date TIMESTAMP,
+	category_id VARCHAR(10) REFERENCES category(category_id),
+	subcategory_id VARCHAR(10) REFERENCES subcategory(subcategory_id)
+)
+
+SELECT * FROM category;
+SELECT * FROM subcategory;
+SELECT * FROM contacts;
+SELECT * FROM campaign;
